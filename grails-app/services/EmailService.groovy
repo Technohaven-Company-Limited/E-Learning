19905ea7c84f83025f7c52e033ea30a275493072
Mohaiminul009 <m.moshi009@gmail.com>
2023-04-11 10:29:00 +0600

    Mohaiminul-11/04/23

---
+++ grails-app/services/EmailService.groovy
@@ -0,0 +1,17 @@
+import elearning.Email
+import elearning.User
+import grails.gorm.services.Service
+
+@Service(Email)
+interface EmailService {
+
+    Email get(Serializable id)
+
+//    List<Email> list(Map args)
+//
+//    Long count()
+
+    void delete(Serializable id)
+
+    Email save(Email email)
+}
